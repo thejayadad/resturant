7c69e101860e6c213aaa5ce11ca8c3a1e01a4458
thejayadad <thejayadad@gmail.com>
2023-09-12 21:21:29 -0400

    update

--- myapp/app/api/auth/[...nextauth]/route.js
+++ myapp/app/api/auth/[...nextauth]/route.js
@@ -1,50 +1,20 @@
-import NextAuth from "next-auth";
+import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import clientPromise from "@/lib/mongodb";
-import { connectMongoDB } from "@/lib/mongodb";
 
 
 const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-  callbacks: {
-    async signIn({ user, account }) {
-      if (account.provider === "google") {
-        const { name, email } = user;
-        try {
-          await connectMongoDB();
-          const userExists = await User.findOne({ email });
 
-          if (!userExists) {
-            const res = await fetch("http://localhost:3000/api/user", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                name,
-                email,
-              }),
-            });
+    providers: [
+        GoogleProvider({
+          clientId: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        })
+      ],
+      adapter: MongoDBAdapter(clientPromise),
 
-            if (res.ok) {
-              return user;
-            }
-          }
-        } catch (error) {
-          console.log(error);
-        }
-      }
-
-      return user;
-    },
-  },
-};
+}
 
 const handler = NextAuth(authOptions);
 
