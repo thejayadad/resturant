0e81a44a8e70a590c7ecfdaf1167c96982c59488
thejayadad <thejayadad@gmail.com>
2023-09-17 13:05:35 -0400

    update

--- myapp/components/Footer.js
+++ myapp/components/Footer.js
@@ -26,7 +26,7 @@ const Footer = () => {
         </div>
 
         <div className="text-xl">
-          <h3 className="text-2xl font-bold mb-4">Follow Us</h3>
+          <h3 className="text-xl font-bold mb-4">Follow Us</h3>
           <div className="flex gap-4">
             <a
               href="#"
