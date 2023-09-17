c3ff523c5968672a70a8567f10d5aab9b9568791
thejayadad <thejayadad@gmail.com>
2023-09-17 10:23:38 -0400

    update

--- myapp/app/page.js
+++ myapp/app/page.js
@@ -1,9 +1,8 @@
-import Hero from "@/components/Hero/Hero";
 
 export default function Home() {
   return (
     <main>
-      <Hero />
+      homePage
     </main>
   )
 }
--- myapp/components/Navbar/Navbar.js
+++ myapp/components/Navbar/Navbar.js
@@ -1,93 +1,39 @@
 'use client'
-import React, { useState } from 'react';
-import { useSession, signOut } from 'next-auth/react';
-import { FaShoppingBag } from 'react-icons/fa';
-import styled from 'styled-components';
-import { mobile } from "../../responsive.js";
-import Link from 'next/link';
 
-const Nav = styled.div`
-  height: 60px;
-  ${mobile({ height: "50px" })}
-`;
+import Link from 'next/link'
+import React, { useState } from 'react'
+import {signIn, signOut, useSession} from 'next-auth/react'
 
-const Container = styled.div`
-  padding: 10px 20px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  ${mobile({ padding: "10px 0px" })}
-`;
-
-const Logo = styled.div`
-  flex: 1;
-  display: flex;
-  align-items: center;
-`;
-
-const MenuButton = styled.div`
-  cursor: pointer;
-  display: block; /* Show on mobile screens */
-  ${mobile({ display: "none" })} /* Hide on desktop screens */
-`;
-
-const MenuItems = styled.div`
-  flex: 1;
-  align-items: center;
-  display: flex;
-  justify-content: space-between;
-  ${mobile({ fontSize: "12px", marginLeft: "10px", display: (props) => (props.open ? "block" : "none") })}
-`;
-
-
-const ActionItems = styled.div`
-  flex: 1;
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  ${mobile({ flex: 2, justifyContent: "center" })}
-`;
 
 const Navbar = () => {
-  const { data: session } = useSession();
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const menuLinks = [
-    { name: 'Home', link: '/' },
-    { name: 'About', link: '/about' },
-    { name: 'Contact', link: '/contact' },
-    { name: 'Gallery', link: '/gallery' },
-  ];
-
-  const handleSignOut = () => {
-    signOut();
-  };
-
+    const {data: session} = useSession()
   return (
-    <Nav>
-      <Container>
-        <Logo>DaMarket Spot</Logo>
-        <MenuButton onClick={() => setMenuOpen(!menuOpen)}>
-          {menuOpen ? (
-            <span>&#215;</span>
-          ) : (
-            <span>&#8801;</span>
-          )}
-        </MenuButton>
-        <MenuItems open={menuOpen}>
-          {menuLinks.map((menuLink) => (
-            <Link href={menuLink.link} key={menuLink.name}>
-              {menuLink.name}
-            </Link>
-          ))}
-        </MenuItems>
-        <ActionItems>
-          <FaShoppingBag />
-          <button onClick={() => signOut()}>SignOut</button>
-        </ActionItems>
-      </Container>
-    </Nav>
-  );
-};
+    <div className="navbar bg-base-100">
+  <div className="flex-1">
+    <a className="btn btn-ghost normal-case text-xl">DaMarket</a>
+  </div>
+  <div className="flex-none">
+    <div className="dropdown dropdown-end">
+      <label tabIndex={0} className="btn btn-ghost btn-circle">
+        <div className="indicator">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+          <span className="badge badge-sm indicator-item">8</span>
+        </div>
+      </label>
+      <div tabIndex={0} className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
+        <div className="card-body">
+          <span className="font-bold text-lg">8 Items</span>
+          <span className="text-info">Subtotal: $999</span>
+          <div className="card-actions">
+            <button className="btn btn-primary btn-block">View cart</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <button onClick={() => {signOut()}}>Logout</button>
+  </div>
+</div>
+  )
+}
 
-export default Navbar;
+export default Navbar
